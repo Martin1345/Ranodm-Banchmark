7dc9625abe6e507f8a300cd796fa8b2a7e0144c1
Martin1345 <69191598+Martin1345@users.noreply.github.com>
2025-07-24 13:06:45 +0200

    Update RandomBenchmark.java

--- RandomBenchmark.java
+++ RandomBenchmark.java
@@ -1,16 +1,16 @@
 package random;
 
 import java.security.SecureRandom;// Import der Klasse SecureRandom, die für kryptografisch sichere Zufallszahlen verwendet wird.
-import java.util.Random;// Klassenimport für die Random-Klasse, die Zufallszahlen generiert
-public class RandomBenchmark{// Diese Klasse führt einen Benchmark-Test für die Klassen Random und SecureRandom durch, um die Leistung zu vergleichen
+import java.util.Random;//  Import der Klasse Random, die Zufallszahlen generiert
+public class RandomBenchmark{// Diese Klasse führt einen Benchmark-Test für die Klassen Random und SecureRandom durch, um die Laufzeiten zu vergleichen
     public static void main(String[] args) {
-        Random random = new Random();// Erzeugung einer Instanz der Random-Klasse, die Zufallszahlen generiert mit einem Standard-Seed
+        Random random = new Random();// Erzeugung einer Instanz der Random-Klasse, die Zufallszahlen generiert, mit einem Standard-Seed
         long Startzeit = System.nanoTime();// Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Startzeit der Schleife zu kennen
         for (int i = 0; i < 1000000; i++) {// Schleife zur Ausführung von 1.000.000 Iterationen zur Messung der Laufzeit der Random-Klasse
             random.nextInt();// In jeder Iteration wird mittels der Methode nextInt() eine neue Zufallszahl generiert
         }
-        long Endzeit = System.nanoTime();// Erneuter Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Endzeit der  Schleife zu kennen
-        long Dauer = Endzeit - Startzeit;// Berecdhnung der Laufzeit des Schleifenblocks, indem die Endzeit von der Startzeit subtrahiert wird
+        long Endzeit = System.nanoTime();// Erneuter Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Endzeit der Schleife zu kennen
+        long Dauer = Endzeit - Startzeit;// Berechnung der Laufzeit des Schleifenblocks, indem die Endzeit von der Startzeit subtrahiert wird
         System.out.println(" Random Dauer: " + Dauer + " Nanosekunden");// Ausgabe des Ergebnisses der Laufzeit in Nanosekunden auf der Konsole
 
         SecureRandom secureRandom = new SecureRandom();// Erzeugung einer Instanz der SecureRandom-Klasse, 
@@ -19,7 +19,7 @@ public class RandomBenchmark{// Diese Klasse führt einen Benchmark-Test für di
         for (int i = 0; i < 1000000; i++) {// Schleife zur Ausführung von 1.000.000 Iterationen zur Messung der Laufzeit der SecureRandom-Klasse
             secureRandom.nextInt();// In jeder Iteration wird mittels der Methode nextInt() eine neue kryptografisch sichere Zufallszahl generiert
         }
-        Endzeit = System.nanoTime();// Erneuter Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Endzeit der der Schleife zu kennen
+        Endzeit = System.nanoTime();// Erneuter Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Endzeit der Schleife zu kennen
         Dauer = Endzeit - Startzeit;// Berechnung der Laufzeit des Schleifenblocks, indem die Endzeit von der Startzeit subtrahiert wird
         System.out.println(" SecureRandom Dauer: " + Dauer + " Nanosekunden");// Ausgabe des Ergebnisses der Laufzeit in Nanosekunden auf der Konsole
     }
