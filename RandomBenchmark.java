d9ba846a65ff5e4fb4c2449df54bdc806c0ddab9
Martin1345 <69191598+Martin1345@users.noreply.github.com>
2025-07-24 12:44:22 +0200

    Update RandomBenchmark.java

--- RandomBenchmark.java
+++ RandomBenchmark.java
@@ -6,8 +6,7 @@ public class RandomBenchmark{// Diese Klasse führt einen Benchmark-Test für di
     public static void main(String[] args) {
         Random random = new Random();// Erzeugung einer Instanz der Random-Klasse, die Zufallszahlen generiert mit einem Standard-Seed
         long Startzeit = System.nanoTime();// Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Startzeit der Schleife zu kennen
-        for (int i = 0; i < 1000000; i++) {// Schleife zur Ausführung von 1.000.000 Iterationen zur Messung der Laufzeit und des Speicherverbrauchs 
-            //der Random-Klasse
+        for (int i = 0; i < 1000000; i++) {// Schleife zur Ausführung von 1.000.000 Iterationen zur Messung der Laufzeit der Random-Klasse
             random.nextInt();// In jeder Iteration wird mittels der Methode nextInt() eine neue Zufallszahl generiert
         }
         long Endzeit = System.nanoTime();// Erneuter Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Endzeit der  Schleife zu kennen
@@ -17,8 +16,7 @@ public class RandomBenchmark{// Diese Klasse führt einen Benchmark-Test für di
         SecureRandom secureRandom = new SecureRandom();// Erzeugung einer Instanz der SecureRandom-Klasse, 
         //die kryptografisch sichere Zufallszahlen generiert
         Startzeit = System.nanoTime();// Abruf der Startzeit für die SecureRandom-Schleife in Nanosekunden
-        for (int i = 0; i < 1000000; i++) {// Schleife zur Ausführung von 1.000.000 Iterationen zur Messung der Laufzeit 
-            //und des Speicherverbrauchs der SecureRandom-Klasse
+        for (int i = 0; i < 1000000; i++) {// Schleife zur Ausführung von 1.000.000 Iterationen zur Messung der Laufzeit der SecureRandom-Klasse
             secureRandom.nextInt();// In jeder Iteration wird mittels der Methode nextInt() eine neue kryptografisch sichere Zufallszahl generiert
         }
         Endzeit = System.nanoTime();// Erneuter Abruf und Speicherung der aktuellen Zeit in Nanosekunden, um die Endzeit der der Schleife zu kennen
